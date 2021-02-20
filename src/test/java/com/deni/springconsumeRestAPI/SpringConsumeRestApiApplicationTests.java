a915923181a0381fffa77fcc40ed71f205cc7bf6
Nonton Anime <artdengun@gmail.com>
2021-02-20 09:23:50 +0700

    consume rest api menggunakan restTemplate

---
+++ src/test/java/com/deni/springconsumeRestAPI/SpringConsumeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deni.springconsumeRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConsumeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
